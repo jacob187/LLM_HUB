98a06c474f5032f7f07a0aea30ecc2d60d352e3f
Jacob Cohen <79770350+jacob187@users.noreply.github.com>
2024-10-03 11:34:39 -0400

    Refactor base_llm to remove pydantic and only use abstract class

--- llm_app/backend/llms/base_llm.py
+++ llm_app/backend/llms/base_llm.py
@@ -1,37 +1,48 @@
 from abc import ABC, abstractmethod
-from typing import List
-
-from pydantic import BaseModel, Field, PrivateAttr
+from typing import List, Dict, Tuple
 
 
-class BaseLLM(ABC, BaseModel):
+class BaseLLM(
+    ABC,
+):
     """
     Base class for all LLMs.
     """
 
-    provider: str = Field(description="The provider of the LLM.")
-    model: str = Field(description="The model to use.")
-    available_models: List[str] = Field(
-        frozen=True, description="The available models."
-    )
-    temperature: float = Field(
-        default=0.7,
-        description="The temperature, degree of randomness or creativity, of the model.",
-    )
-    # TODO: Validate max_tokens based on the model
-    max_tokens: int = Field(
-        default=1024, description="The maximum number of tokens to generate."
-    )
-
-    _api_key: str = PrivateAttr()
+    def __init__(
+        self,
+        provider: str,
+        model: str,
+        available_models: List[str],
+        max_tokens: int,
+        _api_key: str,
+        temperature: float = 0.7,
+    ):
+        self.provider = provider
+        self.model = model
+        self.available_models = available_models
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self._api_key = _api_key
 
     def set_api_key(self, value: str) -> None:
         self._api_key = value
 
-    @abstractmethod
-    def get_available_models(self) -> Dict[str, int]:
-        pass
+    def set_model(self, model: str) -> None:
+        self.model = model
 
     @abstractmethod
-    def initialize_available_models(self) -> None:
-        pass
+    def get_available_models(self) -> Dict[str, int]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def normalize_temperature(self, temperature_range: Tuple(int, int)) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_max_tokens(self, max_tokens: int) -> float:
+        raise NotImplementedError
+
+    @abstractmethod
+    def generate_response(self, prompt: str) -> str:
+        raise NotImplementedError
