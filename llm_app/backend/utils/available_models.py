e5b1ffc8baacdbf98f4ef00243871e6d5a453cb2
Jacob Cohen <79770350+jacob187@users.noreply.github.com>
2024-10-05 17:45:12 -0400

    Add OpenAI models to available_models

---
+++ llm_app/backend/utils/available_models.py
@@ -0,0 +1,20 @@
+OPENAIMODELS = {
+    "GPT-4o": {
+        "api": "gpt-4o-latest",
+        "max_output": 128000,
+    },
+    "GPT-4o Mini": {
+        "api": "gpt-4o-mini-2024-07-18",
+        "max_output": 128000,
+    },
+}
+
+ANTHROPICMODELS = {
+    "Claude 3.5 Sonnet": {
+        "api": "claude-3-5-sonnet-20240620",
+        "max_output": 8192,
+    },
+    "Claude 3 Opus": {"api": "claude-3-opus-20240229", "max_output": 4096},
+    "Claude 3 Sonnet": {"api": "claude-3-sonnet-20240229", "max_output": 4096},
+    "Claude 3 Haiku": {"api": "claude-3-haiku-20240307", "max_output": 4096},
+}
