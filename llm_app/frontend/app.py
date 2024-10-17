8594b0ec2026e0307d0ed1a5c37d437ee2cd329f
Jacob Cohen <79770350+jacob187@users.noreply.github.com>
2024-10-17 12:01:47 -0400

    Fix "New Chat" button app.py.

--- llm_app/frontend/app.py
+++ llm_app/frontend/app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 from llm_app.backend.llms.llm_factory import LLMFactory
 from llm_app.frontend.components.select_model import select_model
 from llm_app.frontend.components.select_settings import select_settings
@@ -15,17 +14,21 @@ st.set_page_config(
 def main():
     st.title("LLM HUB")
 
-    # Initialize session state for model and settings
+    # Initialize session state
     if "user_model" not in st.session_state:
         st.session_state.user_model = None
     if "user_llm" not in st.session_state:
         st.session_state.user_llm = None
     if "temperature" not in st.session_state:
-        st.session_state.temperature = 0.7  # Default value
+        st.session_state.temperature = 0.7
     if "max_tokens" not in st.session_state:
-        st.session_state.max_tokens = 1000  # Default value
+        st.session_state.max_tokens = 1000
+    if (
+        "messages" not in st.session_state
+    ):  # Session state for chat history in chat_box.py
+        st.session_state.messages = []
 
-    # Model selection and settings
+    # Initial model selection and settings
     if st.session_state.user_model is None:
         st.session_state.user_model = select_model()
         merged_models = LLMFactory.merge_models()
@@ -37,7 +40,7 @@ def main():
             user_model=st.session_state.user_model
         )
     else:
-
+        # Removes the selectbox after the user has selected a model
         st.subheader(f"Model: {st.session_state.user_model}")
         merged_models = LLMFactory.merge_models()
         user_model_max_tokens = merged_models[st.session_state.user_model]["max_output"]
@@ -46,12 +49,11 @@ def main():
         )
 
     # Display chat box with current settings
-    if chat_box(
+    user_input = chat_box(
         st.session_state.user_llm,
         st.session_state.temperature,
         st.session_state.max_tokens,
-    ):
-        st.session_state.prompt_submitted = True
+    )
 
     # Button to start a new chat
     if st.button("New Chat"):
@@ -59,6 +61,8 @@ def main():
         st.session_state.user_llm = None
         st.session_state.temperature = 0.7
         st.session_state.max_tokens = 1000
+        st.session_state.messages = []  # Clear messages
+        st.rerun()  # Force a rerun to update the UI
 
 
 if __name__ == "__main__":
