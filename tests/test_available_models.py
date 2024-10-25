2b6179c8dfcfbbdb07a44ba7e871823c461547d1
Jacob Cohen <79770350+jacob187@users.noreply.github.com>
2024-10-25 15:37:29 -0400

    Refactor available_models to use .json file to keep track of API model data.

---
+++ tests/test_available_models.py
@@ -0,0 +1,5 @@
+import llm_app.backend.utils.available_models
+
+
+print("OpenAI Models:", llm_app.backend.utils.available_models.OPENAIMODELS)
+print("Anthropic Models:", llm_app.backend.utils.available_models.ANTHROPICMODELS)
