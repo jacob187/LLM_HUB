eaa16bf8c30c2c84b53bebcf7e51c41d04c3d1eb
Jacob Cohen <79770350+jacob187@users.noreply.github.com>
2024-10-27 17:48:52 -0400

    Create abstract methods for memory wrapper.

--- llm_app/backend/memory/base_memory.py
+++ llm_app/backend/memory/base_memory.py
@@ -1,10 +1,25 @@
 from abc import ABC, abstractmethod
+from typing import Any, Optional
 
 
 # TODO: consider implantation; ConversationBufferMemory, Chains, Vector-store, langgraph
 # ConversationTokenBufferMemory
 class BaseMemoryWrapper(ABC):
 
+    def __init__(self):
+        self.memory = self._create_memory()
+
     @abstractmethod
-    def _create_memory(self):
-        pass
+    def _create_memory(self) -> Any:
+        """Create memory instance"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def add_user_message(self, message: str) -> None:
+        """Add user message to memory"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_memory(self) -> Optional[Any]:
+        """Return memory"""
+        return self.memory
